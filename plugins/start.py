f2eda991b57fdfc50a32ed57d4ac3aa619dfb2f1
animemadness109 <yashpacholi89@gmail.com>
2024-06-20 07:53:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Anime_Madness')
+                    InlineKeyboardButton('🍁 Anime Channel', url='https://t.me/Anime_Madness')
                 ]
             ]
         )

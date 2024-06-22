76d12895a7d0478572295817e44b6fbd27c0cd49
animemadness109 <yashpacholi89@gmail.com>
2024-06-22 17:22:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,6 @@
 #(©)Codexbotz
 
-from pyrogram import version
+from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
